787c347fe72cce2a227cc2c3dfc6d4fe1b5df889
David-Bosnic <davidbosnic99@gmail.com>
2025-01-02 14:25:24 +0100

    Completed Book Bot

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-with open("./books/frankenstein.txt") as f:
-    file_contents = f.read()
+def main():
+    with open("./books/frankenstein.txt") as f:
+        file_contents = f.read()
 
-word_count = len(file_contents.split())
+    print("---   Begin Report   ---")
+    print()
+    print("Word Count: " , word_counter(file_contents))
+    # print()
+    # print("Unique char: ",  unique_char_counter(file_contents))
+    print()
+    print_char_count(unique_char_counter(file_contents))
+    print()
+    print("---   End Report   ---")
 
-print(word_count)
+def word_counter(string):
+    return len(string.split())
+
+def unique_char_counter(string):
+    lower = string.lower()
+    char_counted = {}
+    for char in lower:
+        if char in char_counted:
+            char_counted[char] += 1
+        else:
+            char_counted[char] = 1
+    return char_counted
+
+
+def print_char_count(char_dic):
+    for key in char_dic:
+        if key.isalpha():
+            print(f"The '{key}' character was found {char_dic[key]} times")
+
+
+
+main()
